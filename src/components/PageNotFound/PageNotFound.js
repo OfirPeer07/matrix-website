7cdb6c0045d8eaab8ed66a7fe95602a6c6372412
unknown <ofirpeer07@google.com>
2025-08-31 00:50:58 +0300

    commit

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,46 +1,50 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import './PageNotFound.css';
-import backgroundVideo from './backgroundMatrix.mp4';
 import OFAiRImage from './OFAiR.png';
 
 const PageNotFound = () => {
-  const avatarRef = useRef();
+  const avatarRef = useRef(null);
 
-  const handleClick = () => {
+  const goHome = useCallback(() => {
     if (avatarRef.current) {
       avatarRef.current.classList.add('blink-out');
       setTimeout(() => {
         window.location.href = '/';
-      }, 600);
+      }, 500);
+    } else {
+      window.location.href = '/';
+    }
+  }, []);
+
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault();
+      goHome();
     }
   };
 
   return (
-    <div className="page-not-found-container">
-
-      <video
-        className="background-video"
-        src={backgroundVideo}
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
-
+    <div className="page-not-found-container" role="main" aria-labelledby="pnf-heading">
       {/* OFAiR Avatar + Tooltip */}
       <div
         ref={avatarRef}
         className="OFAiR clickable-avatar"
         style={{ backgroundImage: `url(${OFAiRImage})` }}
-        onClick={handleClick}
+        onClick={goHome}
+        onKeyDown={onKeyDown}
         role="button"
-        aria-label="Click to return to homepage"
+        tabIndex={0}
+        aria-label="Go back to the homepage"
       >
-        <div className="tooltip-homepage">Return to safety</div>
+        <div className="tooltip-homepage" role="tooltip" id="home-tip">
+          Return to safety
+        </div>
       </div>
 
-      {/* Text & 404 */}
-      <div className="text">
+      {/* Text layers */}
+      <h1 id="pnf-heading" className="sr-only">404 Error: Page not found</h1>
+
+      <div className="text text-back">
         <span className="line flickerJunior">JUNIOR&nbsp;&nbsp;&nbsp;submitted</span><br />
         <span className="line flickerCompany">COMPANY&nbsp;&nbsp;&nbsp;committed</span><br />
         <span className="line flickerPage">PAGE&nbsp;&nbsp;&nbsp;omitted</span><br /><br />
@@ -49,13 +53,14 @@ const PageNotFound = () => {
         <span className="_0">0</span>
         <span className="_4_4">4</span>
 
-        <span className="spacer">&nbsp;&nbsp;&nbsp;</span>
+        <span className="spacer" aria-hidden="true">&nbsp;&nbsp;&nbsp;</span>
 
         <span className="blink err_r">Err</span>
         <span className="_0">0</span>
         <span className="blink err_r">r</span>
       </div>
-      <div className="text-two">
+
+      <div className="text text-front" aria-hidden="true">
         <span className="line flickerJunior">JUNIOR&nbsp;&nbsp;&nbsp;submitted</span><br />
         <span className="line flickerCompany">COMPANY&nbsp;&nbsp;&nbsp;committed</span><br />
         <span className="line flickerPage">PAGE&nbsp;&nbsp;&nbsp;omitted</span><br /><br />
@@ -64,14 +69,13 @@ const PageNotFound = () => {
         <span className="_0">0</span>
         <span className="_4_4">4</span>
 
-        <span className="spacer">&nbsp;&nbsp;&nbsp;</span>
+        <span className="spacer" aria-hidden="true">&nbsp;&nbsp;&nbsp;</span>
 
         <span className="blink err_r">Err</span>
         <span className="_0">0</span>
         <span className="blink err_r">r</span>
       </div>
     </div>
-
   );
 };
 
